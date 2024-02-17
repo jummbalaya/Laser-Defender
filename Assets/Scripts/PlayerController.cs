9f88472bf3b91cc3e0ad4ab34d07e39d973e193c
jummbalaya <mite.shterjov@gmail.com>
2024-02-17 16:10:42 +0100

    added screen boundaries, enemy paths monoscript

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5.0f;
+
+    [SerializeField] float paddingLeft = 1f;
+    [SerializeField] float paddingRight = 1f;
+    [SerializeField] float paddingTop = 5.0f;
+    [SerializeField] float paddingBottom = 2.0f;
+
+
+    Vector2 rawInput;
+
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
+    private void Start()
+    {
+        InitBounds();
+    }
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+
+        // limit the player to not leave the screen
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+        transform.position = newPos;
+    }
+
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+        Debug.Log(rawInput);
+    }
+
+    // init the bounding vectors that will limit the player to not leave the screen
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0,0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+}
---
+++ Assets/Scripts/WaveConfigSO.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
+public class WaveConfigSO : ScriptableObject
+{
+    [SerializeField] Transform pathPrefab;
+    [SerializeField] float moveSpeed = 5.0f;
+
+    public Transform GetStartingWaypoint()
+    {
+        return pathPrefab.GetChild(0);
+    }
+
+    public List<Transform> GetWaypoints()
+    {
+        List<Transform> waypoints = new List<Transform>();
+        foreach (Transform child in pathPrefab)
+        {
+            waypoints.Add(child);
+        }
+
+        return waypoints;
+    }
+
+    public float GetMoveSpeed()
+    {
+        return moveSpeed;
+    }
+}
